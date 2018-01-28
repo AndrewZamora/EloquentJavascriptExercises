27b8b2c42a81ee9ab9bd4f4f14a79d3274286f00
AndrewZamora <andrewmichaelzamora@gmail.com>
2018-01-28 22:51:14 +0900

    Added fizzbuzz Exercise

--- Ch2_loopingTriangle.js
+++ Ch2_loopingTriangle.js
@@ -1,3 +1,14 @@
+// Chapter 2: Looping a triangle
+// Write a loop that makes seven calls to console.log to output the following triangle:
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+// It may be useful to know that you can find the length of a string by writing .length after it.
+
 var string = "########";
 
 for (var i = 0; i < string.length; i++) {
